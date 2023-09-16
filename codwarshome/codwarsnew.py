f8178ae8bd73539ace684aa30082e471a0f7096f
FLUID1337 <dimasavelyev@icloud.com>
2023-09-16 10:23:42 +0700

    codwars3

---
+++ codwarshome/codwarsnew.py
@@ -0,0 +1,7 @@
+def get_volume_of_cuboid(length, width, height):
+    return length*width*height
+
+
+    "Бобу нужен быстрый способ вычислить объем кубоида с тремя значениями:" 
+    "length, width и height кубоида."
+    "Напишите функцию, которая поможет Бобу с этим вычислением."
